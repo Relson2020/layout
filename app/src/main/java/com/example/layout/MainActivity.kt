6e5e8b411665c8b7a5a15f90893e6eba739e919f
relinns <sonu.prajapati@relinns.com>
2022-01-05 17:59:14 +0530

    layout

--- app/src/main/java/com/example/layout/MainActivity.kt
+++ app/src/main/java/com/example/layout/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.layout
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.layout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val intent = Intent(this,ListView::class.java)
-        startActivity(intent)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.buttonAdapter.setOnClickListener {
+            val intent = Intent(this,ListView::class.java)
+            startActivity(intent)
+        }
+
+        binding.buttonMotion.setOnClickListener {
+            val intent = Intent(this,MotionLayout::class.java)
+            startActivity(intent)
+        }
     }
 
 
